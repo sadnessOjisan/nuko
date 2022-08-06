11ec8250b3be31c4fd59a18323953594c21c3fbe
sadness_ojisan <sadness.ojisan@gmail.com>
2022-08-06 12:31:48 +0900

    Add test case

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
-type Input = ReadonlyArray<string>;
+type Input = ReadonlyArray<string | number | boolean | Record<string, boolean>>;
 
-export const cn = (input: Input): string => {
-  return input.join(" ");
+type InputForVariadic = ReadonlyArray<Input>;
+
+/**
+ * Cat classname
+ * @param input
+ * @returns
+ */
+export const cn = (...input: Input | InputForVariadic): string => {
+  const isInputVariadicArg = input.length > 1;
+  if (isInputVariadicArg && Array.isArray(input)) {
+    return input.filter((el) => Boolean(el)).join(" ");
+  } else {
+    const nonVariadicInput = input[0];
+    if (Array.isArray(nonVariadicInput)) {
+      return nonVariadicInput.filter((el) => Boolean(el)).join(" ");
+    }
+    if (typeof nonVariadicInput === "object") {
+      return catForObjectInput(nonVariadicInput);
+    }
+  }
+
+  return "";
+};
+
+const catForObjectInput = (input: Object): string => {
+  return Object.entries(input)
+    .filter((set) => {
+      const key = set[0];
+      const value = set[1];
+      return Boolean(value);
+    })
+    .map((set) => set[0])
+    .join(" ");
 };
